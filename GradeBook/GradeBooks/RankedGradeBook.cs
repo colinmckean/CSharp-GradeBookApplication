dfd0760325d5cd06392e34e1f93cd95a360e10ff
Colin McKean <colinmckeandigital@gmail.com>
2019-09-21 18:00:47 +0100

    Add Override methods for Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -24,11 +24,11 @@ namespace GradeBook.GradeBooks
             var grades = Students.OrderByDescending(student => student.AverageGrade)
                                     .Select(e => e.AverageGrade)
                                     .ToList();
-            if (grades[threshold -1] <= averageGrade)
+            if (grades[threshold - 1] <= averageGrade)
             {
                 return 'A';
             }
-            else if (grades[(threshold*2) - 1] <= averageGrade)
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
             {
                 return 'B';
             }
@@ -43,7 +43,29 @@ namespace GradeBook.GradeBooks
             else
             {
                 return 'F';
-            }            
-        }        
+            }
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else if (Students.Count >= 5)
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else if(Students.Count >= 5){
+                base.CalculateStudentStatistics(name);
+            }
+        }
     }
 }
